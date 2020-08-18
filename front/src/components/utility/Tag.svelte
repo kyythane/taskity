cae724c150f69417e656154a9c0c23b18eb5b5bf
Lillian Primrose <kyythane@gmail.com>
2020-08-18 01:07:00 -0400

    Simple tag animation

--- front/src/components/utility/Tag.svelte
+++ front/src/components/utility/Tag.svelte
@@ -1,44 +1,47 @@
 <style>
+    .resizable {
+        transition: width 200ms ease-in-out;
+    }
     :global(.yellow) {
         @apply bg-yellow-400;
-        @apply text-yellow-700;
+        @apply text-yellow-900;
     }
     :global(.pink) {
         @apply bg-pink-400;
-        @apply text-pink-700;
+        @apply text-pink-900;
     }
     :global(.red) {
         @apply bg-red-400;
-        @apply text-red-700;
+        @apply text-red-900;
     }
     :global(.orange) {
         @apply bg-orange-400;
-        @apply text-orange-700;
+        @apply text-orange-900;
     }
     :global(.indigo) {
         @apply bg-indigo-400;
-        @apply text-indigo-700;
+        @apply text-indigo-900;
     }
     :global(.purple) {
         @apply bg-purple-400;
-        @apply text-purple-700;
+        @apply text-purple-900;
     }
     :global(.blue) {
         @apply bg-blue-400;
-        @apply text-blue-700;
+        @apply text-blue-900;
     }
     :global(.teal) {
         @apply bg-teal-400;
-        @apply text-teal-700;
+        @apply text-teal-900;
     }
     :global(.green) {
         @apply bg-green-400;
-        @apply text-green-700;
+        @apply text-green-900;
     }
 </style>
 
 <script lang="ts">
-    export let text: string = '!';
+    export let text: string = 'A long label';
     export let size: 'compact' | 'full' = 'compact';
     export let color:
         | 'yellow'
@@ -54,7 +57,9 @@
 </script>
 
 <div
-    class="{`text-center leading-4 object-contain rounded-full text-sm ${size === 'compact' ? 'w-4' : 'w-16'} h-4 m-1 ${color}`}"
+    on:mouseover="{() => (size = 'full')}"
+    on:mouseout="{() => (size = 'compact')}"
+    class="{`text-center leading-4 object-contain rounded-full text-sm ${size === 'compact' ? 'w-4' : 'w-24'} h-4 m-1 ${color} resizable whitespace-no-wrap overflow-hidden`}"
 >
     {size === 'compact' ? initial : text}
 </div>
